bd5b5f704a73280a72f1e44b2138377aafcc7473
Caedilla <42666479+Caedilla@users.noreply.github.com>
2018-11-16 19:28:07 +0100

    Update

--- Core.lua
+++ Core.lua
@@ -56,6 +56,12 @@ function RFM:GetPvPTalents()
 	PlayerTalentFrameTalentsPvpTalentFrame.TalentSlot3:SetSelectedTalent(talentID)
 end
 
+function RFM.Reload()
+	--ChatFrame1:AddMessage("|cFFFF75B3RSA:|r " .. format(message1) .. "|cffffa0a0|Hrldui:RSA|h[Reload]|h|r" .. format(message2))
+	ChatFrame1:AddMessage("|cFFFF75B3RSA:|r |cffffa0a0|Hrldui:RSA|h[Reload UI]|h|r")
+	--ChatFrame1:AddMessage("|Hlinktype:google.com|hgoogle.com|h")
+end
+
 
 --"Interface/PVPFrame/Icons/UI_RankedPvP_05"
 --"Interface/LFGFRAME/UI-LFG-ICON-ROLES"
@@ -91,11 +97,15 @@ end
 ----------------					Grid2 Adjustments					-------------
 -------------------------------------------------------------------------------------
 local function Grid2LayoutFrameMover()
+	if InCombatLockdown() then return end
 	--Grid2LayoutFrame:SetPoint("BOTTOMRIGHT",UIParent,"BOTTOMRIGHT",-4,210)
-	Grid2LayoutFrame:ClearAllPoints()
-	Grid2LayoutFrame:SetPoint("BOTTOMRIGHT",ChatFrame1,"BOTTOMRIGHT",0,4)
-	--print("Moving Grid2")
-	--RFM.BagnonMonitor:SetScript("OnUpdate", itemFrameMoveOnUpdate)	
+	local a,b,c,d,e = Grid2LayoutFrame:GetPoint()
+	if a == "BOTTOMLEFT" and b == ChatFrame1 and c == "TOPLEFT" and d == -2 and e == 6 then
+		RFM.Grid2Monitor:SetScript("OnUpdate",nil)
+	else 
+		Grid2LayoutFrame:ClearAllPoints()
+		Grid2LayoutFrame:SetPoint("BOTTOMLEFT",ChatFrame1,"TOPLEFT",-2,6)
+	end
 end
 
 -------------------------------------------------------------------------------------
@@ -117,10 +127,12 @@ local function Hider()
 	else
 		--if not MinimapCluster:IsShown() then MinimapCluster:Show() end
 		-- Always hide Blizzard's Raid Frames.
-		CompactRaidFrameManager:UnregisterAllEvents()
-		CompactRaidFrameManager:Hide()
-		CompactRaidFrameContainer:UnregisterAllEvents()
-		CompactRaidFrameContainer:Hide()
+		if CompactRaidFrameManager then
+			CompactRaidFrameManager:UnregisterAllEvents()
+			CompactRaidFrameManager:Hide()
+			CompactRaidFrameContainer:UnregisterAllEvents()
+			CompactRaidFrameContainer:Hide()
+		end
 		
 		-- Always hide Blizzard's Player Frame.
 		PlayerFrame:UnregisterAllEvents()
@@ -199,6 +211,9 @@ local function MyCvars()
 	SetCVar("nameplateOverlapV", 0.5)
 	SetCVar("nameplateShowAll", 1) -- Always show nameplates, rather than just in combat.
 	SetCVar("nameplateMaxDistance", 40)
+	
+	SetCVar("nameplateOverlapH", 1)
+	SetCVar("nameplateOverlapV", 0.1)
 	
 	-- Forces nameplates to stay on screen when the nameplate should be below or above the screen.
 	SetCVar("nameplateOtherBottomInset", -1)
@@ -316,12 +331,37 @@ function RFM:OnEnable()
 	UIWidgetBelowMinimapContainerFrame:ClearAllPoints()
 	UIWidgetBelowMinimapContainerFrame:SetPoint("TOP",0,-60)
 	
+	--Minimap:SetClassBlipTexture([[Interface\Addons\RFM\Media\MinimapClassIcons.tga]])
+	--[[
+	RAID_CLASS_COLORS.DEATHKNIGHT.r = 207/255 RAID_CLASS_COLORS.DEATHKNIGHT.g = 55/255 RAID_CLASS_COLORS.DEATHKNIGHT.b = 77/255
+	RAID_CLASS_COLORS.DEMONHUNTER.r = 153/255 RAID_CLASS_COLORS.DEMONHUNTER.g = 255/255 RAID_CLASS_COLORS.DEMONHUNTER.b = 0/255
+	RAID_CLASS_COLORS.DRUID.r = 255/255 RAID_CLASS_COLORS.DRUID.g = 131/255 RAID_CLASS_COLORS.DRUID.b = 50/255
+	RAID_CLASS_COLORS.HUNTER.r = 161/255 RAID_CLASS_COLORS.HUNTER.g = 186/255 RAID_CLASS_COLORS.HUNTER.b = 73/255
+	RAID_CLASS_COLORS.MAGE.r = 55/255 RAID_CLASS_COLORS.MAGE.g = 197/255 RAID_CLASS_COLORS.MAGE.b = 255/255
+	RAID_CLASS_COLORS.MONK.r = 0/255 RAID_CLASS_COLORS.MONK.g = 169/255 RAID_CLASS_COLORS.MONK.b = 138/255
+	RAID_CLASS_COLORS.PALADIN.r = 255/255 RAID_CLASS_COLORS.PALADIN.g = 99/255 RAID_CLASS_COLORS.PALADIN.b = 183/255
+	RAID_CLASS_COLORS.PRIEST.r = 255/255 RAID_CLASS_COLORS.PRIEST.g = 236/255 RAID_CLASS_COLORS.PRIEST.b = 211/255
+	RAID_CLASS_COLORS.ROGUE.r = 255/255 RAID_CLASS_COLORS.ROGUE.g = 216/255 RAID_CLASS_COLORS.ROGUE.b = 0/255
+	RAID_CLASS_COLORS.SHAMAN.r = 0/255 RAID_CLASS_COLORS.SHAMAN.g = 107/255 RAID_CLASS_COLORS.SHAMAN.b = 195/255
+	RAID_CLASS_COLORS.WARLOCK.r = 150/255 RAID_CLASS_COLORS.WARLOCK.g = 119/255 RAID_CLASS_COLORS.WARLOCK.b = 229/255
+	RAID_CLASS_COLORS.WARRIOR.r = 187/255 RAID_CLASS_COLORS.WARRIOR.g = 127/255 RAID_CLASS_COLORS.WARRIOR.b = 75/255
+	]]--
 	
 	-- Create Backdrops
 	for i=1,10 do
 		local Chat = CreateFrame("Frame","R_ChatFrame"..i,ChatFrameList[i])
 		local Background = Chat:CreateTexture("R_ChatFrame"..i.."Background","BACKGROUND")
 		local AnchorFrame = _G[ChatFrameList[i]:GetName()]
+		
+		
+		local ChatTab = CreateFrame("Frame","R_ChatTab"..i,GeneralDockManager)
+		local TabText = ChatTab:CreateFontString("R_ChatTabText"..i)
+		local text = _G["ChatFrame"..i.."TabText"]
+		local tab = _G["ChatFrame"..i.."Tab"]
+		TabText:SetFont(LSM:Fetch("font", "RUF"),18,"OUTLINE")			
+		TabText:SetText(_G["ChatFrame"..i.."TabText"]:GetText())
+		
+		
 		Chat:SetBackdrop(cfg.Border)
 		Chat:SetBackdropBorderColor(0,0,0,1)
 		Chat:SetFrameLevel(0)
@@ -329,7 +369,8 @@ function RFM:OnEnable()
 		AnchorFrame.ScrollBar:SetPoint("RIGHT", AnchorFrame,24,0)
 		AnchorFrame.ScrollBar:SetHeight(191)
 		AnchorFrame.ScrollToBottomButton:ClearAllPoints()
-		AnchorFrame.ScrollToBottomButton:SetPoint("TOPRIGHT",50000,50000)
+		AnchorFrame.ScrollToBottomButton:SetPoint("TOPRIGHT",50000,50000)		
+		
 		if i == 3 then
 			AnchorFrame.ScrollBar:ClearAllPoints()
 			AnchorFrame.ScrollBar:SetPoint("TOPLEFT", AnchorFrame,-24,0)
@@ -344,6 +385,45 @@ function RFM:OnEnable()
 			Chat:SetPoint("TOPLEFT",AnchorFrame,-2,4)
 			Chat:SetPoint("BOTTOMRIGHT",AnchorFrame,2,-4)
 		end
+
+		
+		if i == 1 then
+			TabText:SetPoint("LEFT",GeneralDockManager,"LEFT",0,0)
+			TabText:SetText(string.upper(text:GetText()))
+			TabText:Show()
+			tab:SetWidth(35)
+			tab:SetHeight(15)
+		elseif i == 2 then
+			TabText:SetPoint("LEFT",_G["R_ChatTabText"..i-1],"RIGHT",0,0)
+			TabText:SetText(string.upper(text:GetText()))
+			TabText:Show()
+			tab:SetWidth(51)
+			tab:SetHeight(15)
+		elseif i == 3 then
+			TabText:SetPoint("LEFT",_G["R_ChatTabText"..i-1],"RIGHT",0,0)
+			TabText:SetText(string.upper(text:GetText()))
+			TabText:Show()
+			tab:SetWidth(45)
+			tab:SetHeight(15)
+		elseif i == 4 then
+			TabText:SetPoint("LEFT",_G["R_ChatTabText"..i-1],"RIGHT",0,0)
+			TabText:SetText(string.upper(text:GetText()))
+			TabText:Show()
+			tab:SetWidth(86)
+			tab:SetHeight(15)
+		--[[elseif i == 5 then
+			TabText:SetPoint("LEFT",_G["R_ChatTabText"..i-1],"RIGHT",0,0)
+			TabText:SetText(string.upper(text:GetText()))
+			TabText:Show()
+			tab:SetWidth(45)
+			tab:SetHeight(15)]]--
+		end
+		
+		GeneralDockManager:ClearAllPoints()
+		GeneralDockManager:SetPoint("TOPLEFT",ChatFrame1,0,17)
+		GeneralDockManager:SetPoint("TOPRIGHT",ChatFrame1,0,17)
+		
+		
 		Background:SetAllPoints(Chat)
 		Background:SetTexture(LSM:Fetch("background", "Solid"))
 		Background:SetVertexColor(0,0,0,0.5)
@@ -366,13 +446,18 @@ function RFM:OnEnable()
 	LFGInvitePopup:SetScript("OnShow", LFGAcceptRole)
 	
 	
-	if Grid2 then
-		local Grid2Monitor = CreateFrame("Frame","RFM_Grid2Monitor")
-		RFM.Grid2Monitor = Grid2Monitor
+	if Grid2 then	
+		local Grid2Monitor = CreateFrame("Frame","RFM_Grid2Monitor")		
+		Grid2Monitor:RegisterEvent("PLAYER_ENTERING_WORLD")
+		--Grid2Monitor:RegisterEvent("GROUP_ROSTER_UPDATE")
+		Grid2Monitor:SetScript("OnUpdate",Grid2LayoutFrameMover)
+		RFM.Grid2Monitor = Grid2Monitor		
 		Grid2LayoutFrame:HookScript("OnShow",Grid2LayoutFrameMover)
+		Grid2LayoutFrame:HookScript("OnSizeChanged",Grid2LayoutFrameMover)
 		if not InCombatLockdown() then
 			Grid2LayoutFrame:Hide()
 			Grid2LayoutFrame:Show()
+			Grid2LayoutFrameMover()
 		end
 	end	
 	if Bagnon then
